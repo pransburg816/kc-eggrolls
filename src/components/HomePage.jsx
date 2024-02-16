59d1f6065eaec77cd37695f02dfc261b6f2cc9aa
pransburg816 <phillip.ransburg@hotmail.com>
2024-02-16 15:27:50 -0600

    Initial commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to MySite</h1>
+      <p>This is the home page of my React website.</p>
+    </div>
+  );
+};
+
+export default HomePage;
